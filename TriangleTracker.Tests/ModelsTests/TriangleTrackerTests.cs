b5cd7ac57c819c176725277b4788340afd8fe7f0
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-08 15:21:48 -0800

    add test and pass test for scalene
    Co-authored-by: David Sterry <sterry.david@gmail.com>
    Diego Salazar <salazardiego343@gmail.com>

--- TriangleTracker.Tests/ModelsTests/TriangleTrackerTests.cs
+++ TriangleTracker.Tests/ModelsTests/TriangleTrackerTests.cs
@@ -14,5 +14,12 @@ namespace TriangleTracker.Tests
       Triangle testType = new Triangle();
       Assert.AreEqual(true, testType.TriangleType(3,3,3));
     } 
+
+    [TestMethod]
+    public void TriangleType_IsScalene_True()
+    {
+      Triangle testType = new Triangle();
+      Assert.AreEqual(true, testType.TriangleType(1,2,3));
+    } 
   }
 }
